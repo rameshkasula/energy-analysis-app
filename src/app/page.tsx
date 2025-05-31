966ab281ab971cb2d39c1e0d090c982c3d0f0695
rameshkasula <rameshlearningadvance@gmail.com>
2025-06-01 02:06:23 +0530

    Remove authentication and user management API routes, along with related components and pages, to streamline the application structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-// "use client";
 
-// import AppHeader from "@/common/app-header";
-// import Banner from "@/components/home/banner";
-// import * as React from "react";
-
-// export default function page() {
-//   return (
-//     <React.Fragment>
-//       <AppHeader />
-//       <Banner />
-//     </React.Fragment>
-//   );
-// }
 
 "use client";
 
@@ -20,22 +7,3 @@ import { redirect } from "next/navigation";
 export default function Page() {
   return redirect("/workspace");
 }
-
-// let's make a server component
-// if user is not logged in, redirect to auth/sign-in
-// if user is logged in, redirect to /workspace
-
-// import { getSession } from "next-auth/react";
-// import { redirect } from "next/navigation";
-
-// export default async function Page() {
-//   const session = await getSession();
-
-//   if (!session) {
-//     redirect("/auth/sign-in");
-//   } else {
-//     redirect("/workspace");
-//   }
-
-//   return null; // Since we're redirecting, there's no need to render anything
-// }
--- src/utils/theme/app-theme.tsx
+++ src/utils/theme/app-theme.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { useBuildTheme } from "./colors";
 import AppSnackBar from "@/common/app-snack-bar";
-import SessionProviders from "@/helpers/session-helper";
 import NextTopLoader from "nextjs-toploader";
+import SessionProviders from "@/helpers/session-helper";
 
 typeof ThemeProvider;
 
@@ -19,18 +19,16 @@ export default function AppThemeProvider({
   const apptheme = useBuildTheme();
   return (
     <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-      <SessionProviders>
-        <ThemeProvider theme={apptheme}>
-          <CssBaseline />
-          <AppSnackBar />
-          <NextTopLoader
-            color={apptheme.palette.primary.main}
-            showSpinner={false}
-            speed={300}
-          />
-          {children}
-        </ThemeProvider>
-      </SessionProviders>
+      <ThemeProvider theme={apptheme}>
+        <CssBaseline />
+        <AppSnackBar />
+        <NextTopLoader
+          color={apptheme.palette.primary.main}
+          showSpinner={false}
+          speed={300}
+        />
+        {children}
+      </ThemeProvider>
     </AppRouterCacheProvider>
   );
 }
